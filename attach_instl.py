6e19415b172329897f9e05ea6b09c2a5bfac855c
y-rakuno <y-rakuno@sakura.ad.jp>
2019-07-26 15:58:23 +0900

    add subprocess run

--- attach_instl.py
+++ attach_instl.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from prometheus_client import make_wsgi_app
 from prometheus_client import Gauge
 from wsgiref.simple_server import make_server
@@ -8,9 +10,15 @@ ATTACH_STATUS = Gauge('attach_status', 'Secure Mobile Connect Attach status')
 attach_status = 0
 
 
+def do_attach():
+    result = subprocess.run(('sudo pon'))
+    print(result)
+
+
 def attach_app(environ, start_fn):
     global attach_status
     if environ['PATH_INFO'] == '/metrics':
+        do_attach()
         attach_status = not attach_status
         ATTACH_STATUS.set(attach_status)
         return metrics_app(environ, start_fn)
