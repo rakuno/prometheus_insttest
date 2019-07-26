f172235d9edd460316400beb95336c67e38f9419
y-rakuno <y-rakuno@sakura.ad.jp>
2019-07-26 14:34:07 +0900

    add ATTACH_STATUS

--- attach_instl.py
+++ attach_instl.py
@@ -1,11 +1,14 @@
 from prometheus_client import make_wsgi_app
+from prometheus_client import Gauge
 from wsgiref.simple_server import make_server
 
 metrics_app = make_wsgi_app()
 
+ATTACH_STATUS = Gauge('attach_status', 'Secure Mobile Connect Attach status')
 
 def attach_app(environ, start_fn):
     if environ['PATH_INFO'] == '/metrics':
+        ATTACH_STATUS.set(1)
         return metrics_app(environ, start_fn)
     start_fn('200 OK', [])
     return [b'Hello Prometheus']
