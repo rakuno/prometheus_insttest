f4e2ab34bbb5c51654d6cc810a5d39f1d233c321
y-rakuno <y-rakuno@sakura.ad.jp>
2019-07-26 14:59:40 +0900

    add ATTACH_STATUS

--- attach_instl.py
+++ attach_instl.py
@@ -5,15 +5,22 @@ from wsgiref.simple_server import make_server
 metrics_app = make_wsgi_app()
 
 ATTACH_STATUS = Gauge('attach_status', 'Secure Mobile Connect Attach status')
+attach_status = 0
 
-def attach_app(environ, start_fn):
-    if environ['PATH_INFO'] == '/metrics':
-        ATTACH_STATUS.set(1)
-        return metrics_app(environ, start_fn)
-    start_fn('200 OK', [])
-    return [b'Hello Prometheus']
+
+class AttachMonitor:
+    def __init__(self):
+        self.attach_status = 0
+
+    def attach_app(self, environ, start_fn):
+        if environ['PATH_INFO'] == '/metrics':
+            self.attach_status = not self.attach_status
+            ATTACH_STATUS.set(self.attach_status)
+            return metrics_app(environ, start_fn)
+        start_fn('200 OK', [])
+        return [b'Hello Prometheus']
 
 
 if __name__ == '__main__':
-    httpd = make_server('', 8000, attach_app)
+    httpd = make_server('', 8000, AttachMonitor.attach_app)
     httpd.serve_forever()
